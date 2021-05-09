ba596b6a2e0a31e16885fb4fa15e8382211e4c45
Colin Sagan <careof@colinsagan.com>
2021-05-09 21:17:09 +0800

    Add python file with only the code comments.

---
+++ rock-paper-scissors.py
@@ -0,0 +1,12 @@
+# Ask user for their choice
+# (Get user input python 2)
+
+# Generate computer choice
+# (Generate a random choice from three options)
+
+# Compare computer choice to user input
+# I.E. If user said rock and computer said scissors then user wins
+# (Check if two strings are the same)
+
+# Tell user who won
+# (Print message)
