a1c8183f3aa46522c34459b84ab5002639ed117f
Reese Dychiao <reesedychiao@Computers-MacBook-Air-2.local>
2021-05-11 09:39:12 +0800

    added a function

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,29 +1,16 @@
-# Print something to test if it's working.
-# print to screen
 print("hello")
 
-# Ask user for their choice
-# (Get user input python 2)
+
 def play():
 	weapon = raw_input("Rock, paper, or scissors? ")
 	print("You entered " + weapon + "!")
-
-# Generate computer choice
-# (Generate a random choice from three options)
 	import random
 	options = ['rock', 'paper', 'scissors']
 	counter = random.choice(options)
 	print("The computer chooses " + counter + ".")
 
-# Compare computer choice to user input
-# I.E. If user said rock and computer said scissors then user wins
-# (Check if two strings are the same)
-
 	if weapon == counter :
 		print("We have a tie!")
-
-# Tell user who won
-# (Print message)
 	elif weapon == 'rock' and counter == 'paper' :
 		print("Paper beats rock. You lose :(")
 	elif weapon == 'paper' and counter == 'rock' :
@@ -38,6 +25,7 @@ def play():
 		print("Scissors beats paper. You win!")
 	else:
 		print("Try again :(")
+	
 
 	if (weapon == 'exit'):
 		exit()
