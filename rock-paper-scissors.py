ef543f2d82852fc06b2bdefd82e7729d0c30b6e9
Reese Dychiao <reesedychiao@Computers-MacBook-Air-2.local>
2021-05-13 10:24:24 +0800

    tracked score

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -1,35 +1,53 @@
 print("hello")
 
+score = {"user":0, "computer":0}
 
-def play():
+
+def play(score):
 	weapon = raw_input("Rock, paper, or scissors? ")
 	print("You entered " + weapon + "!")
+
+	if (weapon == 'exit'):
+		exit()
+
 	import random
 	options = ['rock', 'paper', 'scissors']
 	counter = random.choice(options)
 	print("The computer chooses " + counter + ".")
 
+
 	if weapon == counter :
 		print("We have a tie!")
 	elif weapon == 'rock' and counter == 'paper' :
-		print("Paper beats rock. You lose :(")
+		print("You lose :(")
+		score["computer"] += 1
 	elif weapon == 'paper' and counter == 'rock' :
-		print("Paper beats rock. You win!")
+		print("You win!")
+		score["user"] += 1
 	elif weapon == 'rock' and counter == 'scissors' :
-		print("Rock beats scissors. You win!")
+		print("You win!")
+		score["user"] += 1
 	elif weapon == 'scissors' and counter == 'rock' :
-		print("Rock beats scissors. You lose :(")
+		print("You lose :(")
+		score["computer"] += 1
 	elif weapon == 'paper' and counter == 'scissors' :
-		print("Scissors beats paper. You lose :(")
+		print("You lose :(")
+		score["computer"] += 1
 	elif weapon == 'scissors' and counter == 'paper' :
-		print("Scissors beats paper. You win!")
+		print("You win!")
+		score["user"] += 1
 	else:
 		print("Try again :(")
-	
 
-	if (weapon == 'exit'):
-		exit()
+	raw_user_score = int(score["user"])
+	user_string = str(raw_user_score)
 
-	play ()
+	raw_computer_score = int(score["computer"])
+	computer_string = str(raw_computer_score)
 
-play()
+	print("Your score: " + user_string + " \nComputer's score: " + computer_string + "")
+
+
+	play (score)
+
+play(score)
